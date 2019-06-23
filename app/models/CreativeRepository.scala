a8368539a493ebe56facd827d76c07e191fdcc4b
ina <dragoalie@gmail.com>
2019-06-23 21:48:18 +0900

    Make a suitable injected constructor

--- app/models/CreativeRepository.scala
+++ app/models/CreativeRepository.scala
@@ -7,13 +7,12 @@ import scala.concurrent.duration.Duration
 import scala.concurrent.{Await, ExecutionContext}
 
 @Singleton
-class CreativeRepository(implicit ec: ExecutionContext) {
+class CreativeRepository @Inject()(implicit ec: ExecutionContext) {
 
-  private val database = Database.forConfig("db.default")
+  private val db = Database.forConfig("db.default")
+  private val query = sql"SELECT id, name FROM creative WHERE is_deliverable = 'yes'".as[(Long, String)]
+  private val f = db.run(query)
 
-  val query = sql"SELECT id, name FROM creative WHERE is_deliverable = 'yes'".as[(Long, String)]
-  val f = database.run(query)
-
-  val creatives = Await.result(f, Duration.Inf).toMap
+  val creatives: Map[Long, String] = Await.result(f, Duration.Inf).toMap
 
 }
