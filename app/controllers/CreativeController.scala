4dcde2e04efd1911ba4e7434af76d0cfb314e071
jang <dragoalie@gmail.com>
2019-06-29 13:52:08 +0900

    Separate functions

--- app/controllers/CreativeController.scala
+++ app/controllers/CreativeController.scala
@@ -1,12 +1,10 @@
 package controllers
 
-import java.util.Random
-
 import factory.RequestFactory
 import javax.inject.{Inject, Singleton}
 import models.DspCreative
 import play.api.Logger
-import play.api.libs.ws.{WSRequest, WSResponse}
+import play.api.libs.ws.WSResponse
 import play.api.mvc._
 import services.CreativeService
 
@@ -17,30 +15,28 @@ import scala.concurrent.duration.Duration
 class CreativeController @Inject()(
   cc: ControllerComponents,
   service: CreativeService,
-  dspRequest: RequestFactory,
-  random: Random) extends AbstractController(cc) {
+  dspRequest: RequestFactory) extends AbstractController(cc) {
 
   private val logger = Logger.logger
+  private val random = new java.util.Random()
 
-  private def toResponse(req: WSRequest): WSResponse = Await.result(req.get, Duration.Inf)
+  /** bidding (dsp) request accepted */
+  def requestsToResponses: Seq[WSResponse] = dspRequest.fakeRequests.map(req => Await.result(req.get, Duration.Inf))
 
-  def creative = Action {
-
-    /** bidding (dsp) request accepted */
-    val responses = dspRequest.fakeRequests.map(toResponse)
-
+  def winnerFromRes(responses: Seq[WSResponse]): String = {
     /** at this time, mocked bid value generated in here. */
     val dspAds = responses.filter(res => service.isDeliverable(res.body))
       .map(res => DspCreative(res.body.toLong, random.nextDouble))
 
     logger.debug(s"bidders : ${dspAds.mkString(",")}")
+    service.auctionByBid(dspAds).toString
+  }
 
-    val winnerId = service.auctionByBid(dspAds).toString
+  def creative = Action {
+    val winnerId = winnerFromRes(requestsToResponses)
 
     logger.debug(s"winner dsp : $winnerId")
-
     Ok(views.html.creative(winnerId))
-
   }
 
 }
